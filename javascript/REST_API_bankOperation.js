be8d108c3f9afca8621ee51762c547248ba57191
lamtan@ <lamtan93@gmail.com>
2017-08-09 02:12:12 +0200

    -->Update, automatically load the listColllab, get and display bank infos on the form

--- javascript/REST_API_bankOperation.js
+++ javascript/REST_API_bankOperation.js
@@ -1,46 +1,73 @@
-function getAllCollabs() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:8080/sgp/api/collaborateurs/", true);
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    	
-    	res = JSON.parse(this.responseText);
-    	var i =1;
-    	var tableData = '<h2>List Collaborateur</h2>';
-    	tableData += '<table class="table table-striped"><tr><th>Id</th><th>Nom</th><th>Prenom</th><th>Email</th><th>Matricule</th><th>Action</th></tr>';
-    	
-    	$.each(res, function(index, data) {
-    	 tableData += '<tr><td>'+i++ +'</td><td>'+data.nom+'</td><td>'+data.prenom+'</td><td>'+data.emailPro+'</td><td>'+data.matricule+'</td><td><a href="#">Choisir</a></td></tr>';
-    	
-    	});
 
-    	$('#myTable').html(tableData);
-    }
-  };
+	
+	$(document).ready(function() {
+	    $.ajax({
+	    	type: "GET",
+	    	dataType: "json",
+	    	url: "http://localhost:8080/sgp/api/collaborateurs/"
+	    }).then(function(data) {
+	    	
+	    	for (var i = 0; i < data.length; i++) {
+	    		
+	    		var tagTDAction = document.createElement("td");
+				var inputValue = document.createElement("input");
+				inputValue.setAttribute('type',"submit");
+				inputValue.setAttribute('value',"Choisir");
+				inputValue.setAttribute('class',"btn btn-info btn-sm");
+				inputValue.setAttribute('id',data[i].matricule);
+				tagTDAction.appendChild(inputValue);
+	    		
+	    		
+	    		
+				var tagTR = document.createElement("tr");
+				
+				var textNodeNom = document.createTextNode(data[i].nom);
+				var tagTDNom = document.createElement("td");
+				tagTDNom.appendChild(textNodeNom);
+		
+				
+				var textNodePrenom = document.createTextNode(data[i].prenom);
+				var tagTDPrenom = document.createElement("td");
+				tagTDPrenom.appendChild(textNodePrenom);
+				var tagTDMatricule = document.createElement("td");
+				var textNodeMatricule = document.createTextNode(data[i].matricule);
+				tagTDMatricule.appendChild(textNodeMatricule);
+				
+				var textNodeId = document.createTextNode(i+1);
+				var tagTDId = document.createElement("td");
+				tagTDId.appendChild(textNodeId);
+				
+				
+				tagTR.appendChild(tagTDId);
+				tagTR.appendChild(tagTDNom);
+				tagTR.appendChild(tagTDPrenom);
+				tagTR.appendChild(tagTDMatricule);
+				tagTR.appendChild(tagTDAction);
+				
+				document.getElementById('placeInfos').appendChild(tagTR);
+				
+				
+				$(inputValue).click(function(){
+					 var matriculeChoisi = $(this).attr("id");
+					 $.get("http://localhost:8080/sgp/api/collaborateurs/"+matriculeChoisi+"/banque",function(data, status ){
+						 document.getElementById("inputBanque").value = data.banque;
+						 document.getElementById("inputBic").value = data.bic;
+						 document.getElementById("inputIban").value = data.iban;
+					 });
+				})
+				
+				
+			}
+	    	
+	    	
+	    	
+	    });
+	});
  
-  xhttp.send();
-}
+	
+
+	
+	
+	
 
 
-function getBankInfos() {
-	  var xhttp = new XMLHttpRequest();
-	  xhttp.open("GET", "http://localhost:8080/sgp/api/collaborateurs/", true);
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	    	
-	    	res = JSON.parse(this.responseText);
-	    	var i =1;
-	    	var tableData = '<h2>List Collaborateur</h2>';
-	    	tableData += '<table class="table table-striped"><tr><th>Id</th><th>Nom</th><th>Prenom</th><th>Email</th><th>Matricule</th><th>Action</th></tr>';
-	    	
-	    	$.each(res, function(index, data) {
-	    	 tableData += '<tr><td>'+i++ +'</td><td>'+data.nom+'</td><td>'+data.prenom+'</td><td>'+data.emailPro+'</td><td>'+data.matricule+'</td><td><a href="#">Choisir</a></td></tr>';
-	    	
-	    	});
-
-	    	$('#myTable').html(tableData);
-	    }
-	  };
-	 
-	  xhttp.send();
-	}
